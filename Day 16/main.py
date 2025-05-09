19778b7f9efa4f794bd67076a50e0af447364d81
Calista Sullivan <calista.sullivan@gmail.com>
2025-05-09 11:54:17 -0700

    Day 16 complete!

--- Day 16/main.py
+++ Day 16/main.py
@@ -13,13 +13,13 @@ while is_on:
     choice = input(f"What would you like? ({options}) ")
     if choice == "off":
         is_on = False
-    # TODO 1 Print Report
+    #Print Report
     elif choice == "report":
         coffee_maker.report()
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        # TODO 2 Check resources sufficient?, # TODO 3 Process coins, #TODO 4 Check transaction successful?
+        #2 Check resources sufficient?, # 3 Process coins, #4 Check transaction successful?
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            # TODO 5 Make Coffee
+            # 5 Make Coffee
             coffee_maker.make_coffee(drink)
